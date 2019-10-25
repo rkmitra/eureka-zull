edaa988c9e17a34a74dd3b4f6e18d78e1373645a
Raj Kamal Mitra <rmitra@altimetrik.com>
2019-10-25 14:47:09 +0530

    .

---
+++ client1/src/main/java/com/example/client1/Client1Controller.java
@@ -0,0 +1,39 @@
+package com.example.client1;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Client1Controller {
+	
+	@Value("${eureka.instance.instanceId}")
+	private String id;
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/message")
+	public ResponseEntity message()
+	{
+		return new ResponseEntity("response from " + id, HttpStatus.OK);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
